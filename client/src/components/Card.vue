967aa1ad4dfc3df3ef4ac3f78bcc529a1d786cf2
Forsties08 <forsties08@gmail.com>
2021-01-07 15:58:33 +0100

    feat: card and card stack

---
+++ client/src/components/Card.vue
@@ -0,0 +1,69 @@
+<template>
+  <div
+    class="inline-block w-32 h-56 m-auto text-3xl font-bold transfor card"
+    :class="'text-' + color + (scale ? ' transform origin-' + origin + ' ' + scale : '')"
+    @click="toggle"
+  >
+    <div class="w-full h-full bg-white border border-gray-200 rounded-lg card-inner">
+      <div class="grid grid-cols-1 front-face place-content-between">
+        <div class="grid grid-cols-2 text-left">
+          <span class="ml-1">{{ symbol }}</span>
+          <div class="mt-2 text-right">
+            <fxemoji-blackdiamondsuit v-if="color === 'diamond'" class="inline-block" />
+            <fxemoji-blackspadesuit v-if="color === 'spade'" class="inline-block -mt-3" />
+            <fxemoji-blackclubsuit v-if="color === 'club'" class="inline-block -mt-3" />
+            <fxemoji-blackheartsuit v-if="color === 'heart'" class="inline-block -mt-3" />
+          </div>
+        </div>
+        <fxemoji-blackdiamondsuit v-if="color === 'diamond'" class="inline-block m-auto text-6xl" />
+        <fxemoji-blackspadesuit v-if="color === 'spade'" class="inline-block m-auto text-6xl" />
+        <fxemoji-blackclubsuit v-if="color === 'club'" class="inline-block m-auto text-6xl" />
+        <fxemoji-blackheartsuit v-if="color === 'heart'" class="inline-block m-auto text-6xl" />
+        <div class="grid grid-cols-2 text-left transform rotate-180">
+          <span class="ml-1">{{ symbol }}</span>
+          <div class="mt-2 text-right">
+            <fxemoji-blackdiamondsuit v-if="color === 'diamond'" class="inline-block" />
+            <fxemoji-blackspadesuit v-if="color === 'spade'" class="inline-block -mt-3" />
+            <fxemoji-blackclubsuit v-if="color === 'club'" class="inline-block -mt-3" />
+            <fxemoji-blackheartsuit v-if="color === 'heart'" class="inline-block -mt-3" />
+          </div>
+        </div>
+      </div>
+
+      <div class="p-1 bg-white rounded-lg h-80 back-face">
+        <div class="w-full h-full bg-black rounded-lg bg-opacity-80" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from 'vue'
+
+const props = defineProps({
+  color: {
+    type: String,
+    require: true,
+  },
+  symbol: {
+    type: String,
+    require: true,
+  },
+  scale: {
+    type: String,
+    require: false,
+  },
+  origin: {
+    type: String,
+    require: false,
+    default: 'top left',
+  },
+})
+
+const toggle = (e) => {
+  const card = e.target.closest('.card')
+  card.classList.toggle('flipped')
+
+  console.log('flipped: ', card.classList.contains('flipped'))
+}
+</script>
---
+++ client/src/components/CardStack.vue
@@ -0,0 +1,25 @@
+<template>
+  <ul class="top-0 left-0 w-40 card-stack">
+    <li
+      v-for="n in 50"
+      :key="n"
+      :style="{ 'margin-top': n / 40 + 'em' }"
+      class="w-32 h-56 bg-white border rounded-lg"
+      :class="n % 2 === 0 ? 'border-gray-200' : 'border-gray-100'"
+    />
+    <li class="w-32 h-56 border border-gray-200 rounded-lg" :style="{ 'margin-top': 50 / 40 + 'em' }" />
+  </ul>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from 'vue'
+
+const props = defineProps({
+  stack: {
+    type: Number,
+    require: true,
+  },
+})
+
+console.log(props.stack)
+</script>
