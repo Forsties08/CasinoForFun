bd2c96ca9046dfca383aee2567b24c2c47030606
Forsties08 <forsties08@gmail.com>
2021-01-07 15:58:20 +0100

    fix: dark mode break text color

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,6 +1,6 @@
 <template>
-  <main class="px-4 py-10 text-center text-gray-700 dark:text-gray-200">
+  <main class="text-center text-gray-700">
     <router-view />
-    <Footer />
+    <!-- <Footer /> -->
   </main>
 </template>
